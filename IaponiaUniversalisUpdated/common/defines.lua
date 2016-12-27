94b6b3b831a19952ac3b303298b9e86d1f7b32a1
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2016-12-27 16:47:56 -0200

    do a pan over kyoto in the main menu, instead of looking at the sea of japan

--- IaponiaUniversalisUpdated/common/defines.lua
+++ IaponiaUniversalisUpdated/common/defines.lua
@@ -1476,18 +1476,18 @@ NMacroBuildColors = {
 },
 
 NFrontend = {
-	CAMERA_LOOKAT_X = 2958.0, 						-- Rotation point in main menu
+	CAMERA_LOOKAT_X = 1858.0, 						-- Rotation point in main menu
 	CAMERA_LOOKAT_Y = 0.0,
-	CAMERA_LOOKAT_Z = 1519.0,
-	CAMERA_LOOKAT_SETTINGS_X = 2958.0, 				-- Rotation point in settings
+	CAMERA_LOOKAT_Z = 1219.0,
+	CAMERA_LOOKAT_SETTINGS_X = 1858.0, 				-- Rotation point in settings
 	CAMERA_LOOKAT_SETTINGS_Y = 0.0,					-- Y is height
-	CAMERA_LOOKAT_SETTINGS_Z = 1519.0,
-	CAMERA_START_X = 2958.0,						-- Initial position in main menu
-	CAMERA_START_Y = 800,							-- Y is height
-	CAMERA_START_Z = 1400.0,
-	CAMERA_END_X = 2958.0,							-- Move to position in main menu
-	CAMERA_END_Y = 900.0,
-	CAMERA_END_Z = 1400.0,
+	CAMERA_LOOKAT_SETTINGS_Z = 919.0,
+	CAMERA_START_X = 1858.0,						-- Initial position in main menu
+	CAMERA_START_Y = 900,							-- Y is height
+	CAMERA_START_Z = 1100.0,
+	CAMERA_END_X = 1858.0,							-- Move to position in main menu
+	CAMERA_END_Y = 800.0,
+	CAMERA_END_Z = 1100.0,
 	CAMERA_MIN_DIST_FOR_ROTATE = 800.0, 			-- Controlls when rotation starts. When camera is close enought it starts
 	CAMERA_MIN_HEIGHT = 50.0,						-- Minimum camera height
 	CAMERA_MAX_HEIGHT = 3000.0,						-- Maximum camera height
@@ -1503,12 +1503,12 @@ NFrontend = {
 	
 	CAMERA_SPEED_IN_MENUS = 0.1,
 	
-	FRONTEND_POS_X = 2958.0,
-	FRONTEND_POS_Y = 900.0,
-	FRONTEND_POS_Z = 1500.0,
-	FRONTEND_LOOK_X = 2958.0,
+	FRONTEND_POS_X = 1858.0,
+	FRONTEND_POS_Y = 700.0,
+	FRONTEND_POS_Z = 900.0,
+	FRONTEND_LOOK_X = 1858.0,
 	FRONTEND_LOOK_Y = 0.0,
-	FRONTEND_LOOK_Z = 1519.0,
+	FRONTEND_LOOK_Z = 919.0,
 	
 	SETTINGS_POS_X = 2958.0,
 	SETTINGS_POS_Y = 551.0,

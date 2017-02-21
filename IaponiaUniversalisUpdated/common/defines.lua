38cc7765c61c9dd7bef94512965ec99107d1e91f
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2017-02-21 02:14:03 -0300

    Reverted because now I know. Revert "disable random new world (by changing the cutoff date, I don't know if there's a more proper way to do it) This reverts commit 25bfe86e42b62beb69789ec94e9f993c878d6511.

--- IaponiaUniversalisUpdated/common/defines.lua
+++ IaponiaUniversalisUpdated/common/defines.lua
@@ -3,7 +3,7 @@ NDefines = {
 NGame = {
 	START_DATE = "1444.11.11",
 	END_DATE = "1821.1.2",
-	MAX_RANDOM_NEW_WORLD = "1.1.1",
+	MAX_RANDOM_NEW_WORLD = "1492.1.1",
 	
 	GREAT_POWER_FRENZY_START = "1700.1.1",	-- Latest date for the start of the Great Power Frenzy (struggle).
 	GREAT_POWER_FRENZY_WARMUP_MONTHS = 600,	-- How many months does it take for the Great Power Frenzy to reach full potency after starting.

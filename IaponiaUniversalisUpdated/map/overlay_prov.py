02d84af07939c5e8c82cbd822b17b3b0f8e43186
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2017-09-03 19:49:40 -0300

    getting close, time to filter out wastelands

--- IaponiaUniversalisUpdated/map/overlay_prov.py
+++ IaponiaUniversalisUpdated/map/overlay_prov.py
@@ -1,24 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import codecs, sys, re, subprocess
+import codecs, sys, re, subprocess, scipy.misc
 #usage: ./tagcheck.py <mod's 00_countries.txt> <vanilla's 00_countries.txt>
 
 def main():
 	provinces = codecs.open("definition.csv", encoding="utf-8").readlines()
 	provinces = [x.strip().split(";") for x in provinces[1:]]
-	for x in provinces:
-		print(x)
-	bmp = subprocess.check_output(['convert', 'provinces.bmp', '-define', 'histogram:unique-colors=true', '-format', '%c', 'histogram:info:-'])
-	colorRegex = re.compile('#[0-9a-zA-Z]{6}')
-	bmp = [x.strip() for x in bmp.decode("utf-8").split("\n")]
 
-	for x in bmp:
-		matched = colorRegex.search(x)
-		if matched:
-			print(matched.group(0))
-			subprocess.check_output(['convert', 'provinces.bmp', '-fill', 'black', '+opaque',
-			''+matched.group(0)+'', 'out_'+matched.group(0)+'.png'])
+	provMap = scipy.misc.imread('provinces.bmp')
+	colorMap = {}
+	for line in range(1, len(provMap)-1):
+		if line%50 == 0:
+			print("line", line, "of", len(provMap))
+		for col in range(1, len(provMap[0])-1):
+			thisColor = (int(provMap[line][col][0]), int(provMap[line][col][1]),
+			 int(provMap[line][col][2]))
+			if thisColor in colorMap:
+				colorMap[thisColor].append((line, col))
+			else:
+				colorMap[thisColor] = [(line, col)]
 
+
+	subprocess.check_output(['convert', 'provinces.bmp', '-scale', '200%', 'newprov.png'])
+	numbers = ['convert', 'newprov.png', '-font', 'FreeSans-Bold', '-pointsize','20']
+	for prov in provinces:
+		if (int(prov[1]), int(prov[2]), int(prov[3])) in colorMap.keys():
+			thisColor = colorMap[(int(prov[1]), int(prov[2]), int(prov[3]))]
+			y = sum([c[0] for c in thisColor])//len(thisColor) * 2
+			x = abs(sum([c[1] for c in thisColor])//len(thisColor) *2 - 4)
+			print(prov[0], provMap[y/2][x/2-2], (int(prov[1]), int(prov[2]), int(prov[3])))
+			if len(thisColor) > 400:
+				numbers += ['-pointsize', '16']
+			else:
+				numbers += ['-pointsize', '10']
+			numbers += ['-draw', "fill white text "+str(x)+","+str(y)+" '"+prov[0]+"'"]
+			numbers += ['-draw', "fill black text "+str(x+1)+","+str(y-1)+" '"+prov[0]+"'"]
+			#print(int(prov[1]), int(prov[2]), int(prov[3]),
+				#colorMap[(int(prov[1]), int(prov[2]), int(prov[3]))])
+
+	subprocess.check_output(numbers + ['newprov.png'])
+	#subprocess.check_output(['convert', 'provinces.bmp', '-font', 'FreeSans-Regular', '-pointsize', '12', '-draw', "fill black text 400,400 '1'", 'newprov.png'])
+	#for color in colorMap:
+		#print((sum([x[0] for x in colorMap[color]])//len(colorMap[color])),
+		#sum([x[1] for x in colorMap[color]])//len(colorMap[color]))
+
+
+	#print(colorMap)
+	#scipy.misc.imsave('newprov.png', newMap)
+
+def neighbors(x, y):
+	return ((x-1, y),(x+1, y),(x,y-1),(x,y+1))
 	# vanillatags = codecs.open(vanillaFile, encoding="utf-8").readlines()
 	# vanillatags = [x.strip() for x in vanillatags]
 	# vanillaTagList = {}
